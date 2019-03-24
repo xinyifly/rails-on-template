02e7f9fe99e40a3fe7c52cd44604f1c5d3cf22a4
Zeyu Chen <xinyifly@live.cn>
2019-03-25 04:52:51 +0800

    chore: gem factory_bot_rails, ~> 5.0
    chore: gem faker, ~> 1.9

--- template.rb
+++ template.rb
@@ -1,3 +1,18 @@
 gem 'irb', '~> 1.0'
 
 gsub_file 'Gemfile', /gem 'sqlite3'$/, "\\0, '~> 1.3.6'"
+
+inject_into_file 'Gemfile', after: "group :development, :test do\n" do <<-'RUBY'
+  gem 'faker', '~> 1.9'
+RUBY
+end
+
+inject_into_file 'Gemfile', after: "group :development, :test do\n" do <<-'RUBY'
+  gem 'factory_bot_rails', '~> 5.0'
+RUBY
+end
+
+inject_into_file 'test/test_helper.rb', after: "class ActiveSupport::TestCase\n" do <<-'RUBY'
+  include FactoryBot::Syntax::Methods
+RUBY
+end
